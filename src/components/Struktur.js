52796a8e0735c24f40a8f003f0433c3d9dafdcc5
Kyp127 <rifkynaufalat471@gmail.com>
2026-07-13 00:52:20 +0700

    fix: ganti DivisionNode ke GroupNode untuk mencegah ReferenceError

--- src/components/Struktur.js
+++ src/components/Struktur.js
@@ -286,7 +286,7 @@ const Struktur = () => {
                      </svg>
                    )}
  
-                   <DivisionNode div={div} />
+                   <GroupNode group={div} />
                  </div>
                ))}
              </div>
